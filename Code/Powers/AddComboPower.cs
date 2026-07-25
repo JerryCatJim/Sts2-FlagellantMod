fe4f3e20b41fc5979dc47a6487995941a7e75233
JerryCatJim <jinnanlin@163.com>
2026-07-25 21:03:42 +0800

    Fix "AddComboPower" won't respond for "RandomEnemy" Attack Card issue.

--- Code/Powers/AddComboPower.cs
+++ Code/Powers/AddComboPower.cs
@@ -1,10 +1,13 @@
-using BaseLib.Utils;
 using Flagellant.Code.Abstract;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace Flagellant.Code.Powers;
 
@@ -18,24 +21,57 @@ public sealed class AddComboPower : FlagellantPowerModel
         HoverTipFactory.FromPower<ComboPower>()
     ];
 
+    private class Data
+    {
+        public readonly Dictionary<CardModel, List<Creature>> playedCards = new Dictionary<CardModel, List<Creature>>();
+    }
+
+    protected override object InitInternalData()
+    {
+        return new Data();
+    }
+
+    public override Task BeforeCardPlayed(CardPlay cardPlay)
+    {
+        if (cardPlay.Card.Owner.Creature != base.Owner)
+        {
+            return Task.CompletedTask;
+        }
+        GetInternalData<Data>().playedCards.TryAdd(cardPlay.Card, new List<Creature>());
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterDamageGiven(PlayerChoiceContext choiceContext, Creature? dealer, DamageResult result, ValueProp props, Creature target, CardModel? cardSource)
+    {
+        if (CombatManager.Instance.IsInProgress && dealer == base.Owner && target != base.Owner
+            && result.TotalDamage > 0
+            && cardSource != null && cardSource.Owner.Creature == base.Owner
+            && cardSource.Type == CardType.Attack)
+        {
+            if(GetInternalData<Data>().playedCards.TryGetValue(cardSource, out List<Creature>? value) 
+                && value is List<Creature> myList && !myList.Exists((Creature c) => c == target))
+            {
+                myList.Add(target);
+            }
+        }
+        return Task.CompletedTask;
+    }
+
     public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay)
     {
         if (cardPlay == null) return;
 
-        if (cardPlay.Card.Owner.Creature == base.Owner && cardPlay.Card.Type == CardType.Attack)
+        if (GetInternalData<Data>().playedCards.Remove(cardPlay.Card, out List<Creature>? CreatureList) && CreatureList != null)
         {
-            if (cardPlay.Card.TargetType == TargetType.AnyEnemy && cardPlay.Target != null)
+            Flash();
+            foreach (Creature? enemy in CreatureList)
             {
-                Flash();
-                await CommonActions.Apply<ComboPower>(context, cardPlay.Target, cardPlay.Card, 1);
-                await PowerCmd.Decrement(this);
+                if(enemy != null && enemy.IsAlive)
+                {
+                    await PowerCmd.Apply<ComboPower>(context, enemy, 1, Owner, cardPlay.Card);
+                }
             }
-            else if(cardPlay.Card.TargetType == TargetType.AllEnemies)
-            {
-                Flash();
-                await PowerCmd.Apply<ComboPower>(context, base.CombatState.HittableEnemies, 1, Owner, cardPlay.Card);
-                await PowerCmd.Decrement(this);
-            }
+            await PowerCmd.Decrement(this);
         }
     }
 }
