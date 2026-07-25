8064edb1c0286c5914f6479c323535f8dd4f953c
JerryCatJim <jinnanlin@163.com>
2026-07-25 21:03:47 +0800

    Fix AddComboPower won't respond for "RandomEnemy" Attack Card issue.

--- Code/Powers/EndurePower.cs
+++ Code/Powers/EndurePower.cs
@@ -1,8 +1,6 @@
-using BaseLib.Utils;
 using Flagellant.Code.Abstract;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
@@ -25,62 +23,20 @@ public sealed class EndurePower : FlagellantPowerModel
         HoverTipFactory.FromPower<StressPower>()
     ];
 
-    #region GainStressWhenHpLoss
-    /*private class Data
-    {
-        public readonly Dictionary<CardModel, int> playedCards = new Dictionary<CardModel, int>();
-    }
-
-    protected override object InitInternalData()
-    {
-        return new Data();
-    }
-
-    public override Task BeforeCardPlayed(CardPlay cardPlay)
-    {
-        if (cardPlay.Card.Owner.Creature != base.Owner)
-        {
-            return Task.CompletedTask;
-        }
-        if (base.CombatState.CurrentSide != base.Owner.Side)
-        {
-            return Task.CompletedTask;
-        }
-        GetInternalData<Data>().playedCards.Add(cardPlay.Card, 0);
-        return Task.CompletedTask;
-    }*/
-
     public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props, Creature? dealer, CardModel? cardSource)
     {
-        if (CombatManager.Instance.IsInProgress && target == base.Owner 
+        if (CombatManager.Instance.IsInProgress && target == base.Owner
             && result.UnblockedDamage > 0 && base.CombatState.CurrentSide == base.Owner.Side)
         {
-            /*if (cardSource == null || !GetInternalData<Data>().playedCards.ContainsKey(cardSource))
-            {
-                await PowerCmd.Apply<StressPower>(choiceContext, base.Owner, base.Amount, base.Owner, null);
-            }
-            else
-            {
-                GetInternalData<Data>().playedCards[cardSource] += base.Amount;
-            }*/
             await PowerCmd.Apply<StressPower>(choiceContext, base.Owner, base.Amount, base.Owner, null);
         }
     }
-
-    /*public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
-    {
-        if (cardPlay.Card.Owner.Creature == base.Owner && GetInternalData<Data>().playedCards.Remove(cardPlay.Card, out var value))
-        {
-            await CommonActions.ApplySelf<StressPower>(choiceContext, cardPlay.Card, value);
-        }
-    }*/
-    #endregion GainStressWhenHpLoss
 
     public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
     {
         if (player != Owner.Player)
             return;
-        
+
         await PowerCmd.Apply<StressPower>(choiceContext, player.Creature, Amount, player.Creature, ModelDb.Card<Endure>());
         Flash();
     }
