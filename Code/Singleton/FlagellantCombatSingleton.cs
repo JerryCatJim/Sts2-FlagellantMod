2bd824559803a31cc289582dff46c41cce573435
JerryCatJim <jinnanlin@163.com>
2026-08-28 00:27:49 +0800

    Fix some bugs.

--- Code/Singleton/FlagellantCombatSingleton.cs
+++ Code/Singleton/FlagellantCombatSingleton.cs
@@ -4,6 +4,9 @@ using Flagellant.Code.Config;
 using Flagellant.Code.Helper;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.Rooms;
 
 namespace Flagellant.Code.Singleton;
@@ -13,6 +16,33 @@ public class FlagellantCombatSingleton : CustomSingletonModel, IAfterDeathDoor
     public FlagellantCombatSingleton() : base(HookType.Combat)
     {
 
+    }
+
+    public override async Task AfterCurrentHpChanged(Creature creature, decimal delta)
+    {
+        if (DD2Helper.IsFlagellant(creature) && delta > 0 && DD2Helper.WillDieInDoom(creature) != DD2Helper.WillDieInDoom(creature, delta, 0))
+        {
+            FlagellantHelper.ResetAdvancedConditions(null, creature);
+            if (FlagellantHelper.IsInAnyIdle(null, creature))
+            {
+                await CreatureCmd.TriggerAnim(creature, "Revive", 0f);
+            }
+        }
+    }
+
+    public override async Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        if (power is not DoomPower || amount >= 0) return;
+
+        Creature creature = power.Owner;
+        if (DD2Helper.IsFlagellant(creature) && amount < 0 && DD2Helper.WillDieInDoom(creature) != DD2Helper.WillDieInDoom(creature, 0, amount))
+        {
+            FlagellantHelper.ResetAdvancedConditions(null, creature);
+            if (FlagellantHelper.IsInAnyIdle(null, creature))
+            {
+                await CreatureCmd.TriggerAnim(creature, "Revive", 0f);
+            }
+        }
     }
 
     public async Task AfterDeathDoor(Creature creature, decimal healthDelta, decimal powerDelta, DeathDoorType type)
